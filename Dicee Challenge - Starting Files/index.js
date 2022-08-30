98de0ddc4225406c1d9209d0fb5cb61bb3ea19a6
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2022-08-30 18:41:10 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;  //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//for image 2
+var randomNumber2 = Math.floor(Math.random() * 6) +1 ;
+var randomImageSource2= "images/dice"+ randomNumber2+".png"; //2nd image
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩player 1 Wins";
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩player 2 Wins";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
